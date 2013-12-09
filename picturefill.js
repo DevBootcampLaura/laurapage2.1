0db881df48faa2746fa848139d356b809d316e4d
Laura <lleecrowe@gmail.com>
2013-12-09 11:45:43 -0600

    updated picturefill

--- picturefill.js
+++ picturefill.js
@@ -1,20 +1,20 @@
-/*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with span elements). Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 */
+/*! Picturefill - Responsive Images that work today. (and mimic the proposed Picture element with divs). Author: Scott Jehl, Filament Group, 2012 | License: MIT/GPLv2 */
 
 (function( w ){
-
+	
 	// Enable strict mode
 	"use strict";
 
 	w.picturefill = function() {
-		var ps = w.document.getElementsByTagName( "span" );
-
+		var ps = w.document.getElementsByTagName( "div" );
+		
 		// Loop the pictures
 		for( var i = 0, il = ps.length; i < il; i++ ){
 			if( ps[ i ].getAttribute( "data-picture" ) !== null ){
 
-				var sources = ps[ i ].getElementsByTagName( "span" ),
+				var sources = ps[ i ].getElementsByTagName( "div" ),
 					matches = [];
-
+			
 				// See if which sources match
 				for( var j = 0, jl = sources.length; j < jl; j++ ){
 					var media = sources[ j ].getAttribute( "data-media" );
@@ -27,29 +27,22 @@
 			// Find any existing img element in the picture element
 			var picImg = ps[ i ].getElementsByTagName( "img" )[ 0 ];
 
-			if( matches.length ){
-				var matchedEl = matches.pop();
-				if( !picImg || picImg.parentNode.nodeName === "NOSCRIPT" ){
+			if( matches.length ){			
+				if( !picImg ){
 					picImg = w.document.createElement( "img" );
 					picImg.alt = ps[ i ].getAttribute( "data-alt" );
+					ps[ i ].appendChild( picImg );
 				}
-				else if( matchedEl === picImg.parentNode ){
-					// Skip further actions if the correct image is already in place
-					continue;
-				}
-
-				picImg.src =  matchedEl.getAttribute( "data-src" );
-				matchedEl.appendChild( picImg );
-				picImg.removeAttribute("width");
-				picImg.removeAttribute("height");
+				
+				picImg.src =  matches.pop().getAttribute( "data-src" );
 			}
 			else if( picImg ){
-				picImg.parentNode.removeChild( picImg );
+				ps[ i ].removeChild( picImg );
 			}
 		}
 		}
 	};
-
+	
 	// Run on resize and domready (w.load as a fallback)
 	if( w.addEventListener ){
 		w.addEventListener( "resize", w.picturefill, false );
@@ -63,5 +56,5 @@
 	else if( w.attachEvent ){
 		w.attachEvent( "onload", w.picturefill );
 	}
-
+	
 }( this ));
